a7e8f44a31577027a8fdcbc8e8991ac7c898baf4
igor <krzywakigor@gmail.com>
2020-10-25 11:32:39 +0000

    brought redux state into homeScreen.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,5 @@
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants'
+import axios from 'axios'
 
 export const listProducts = () => async (dispatch) => {
     
@@ -13,8 +14,9 @@ export const listProducts = () => async (dispatch) => {
     catch(error){
         dispatch({
             type:PRODUCT_LIST_FAIL,
-            payload:error.response&&error.response.data.message?
-            error.response.data.message:error.message
+            payload:error.response&&error.response.data.message
+            ?error.response.data.message
+            :error.message
         })
     }
 
